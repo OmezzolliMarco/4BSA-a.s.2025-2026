a34cf49ada4a047a322e8d716c422c4af423c7d2
MarcusOme <omezzollimarco@gmail.com>
2025-09-30 09:13:54 +0200

    Lezione del 30/09/2025
    Esercizi svolti in classe

---
+++ Stringhe/prefisoo_suffisso.py
@@ -0,0 +1,19 @@
+def trovaPrefisso(a:str, b:str):
+    lunghezza = min(len(a), len(b))
+    prefisso = ""
+    for i in range(lunghezza):
+        if a[i]==b[i]:
+            prefisso += a[i]
+        else:
+            return prefisso
+
+def trovaSuffisso(a:str, b:str):
+    suffisso = trovaPrefisso(a[::-1], b[::-1])
+    return suffisso[::-1]
+
+
+a = input("Inserisci una parola: ")
+b = input("Inserisci una parola: ")
+
+print(trovaPrefisso(a,b))
+print(trovaSuffisso(a, b))
