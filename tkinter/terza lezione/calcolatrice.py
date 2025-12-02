71db880ba809d5f7c2683b397a0e70d8fcab848f
OmezzolliMarco <omezzollimarco@gmail.com>
2025-12-02 08:50:31 +0100

    Lezione del 02/12/2025

--- tkinter/terza lezione/calcolatrice.py
+++ tkinter/terza lezione/calcolatrice.py
@@ -1,8 +1,42 @@
 import tkinter as tk
 
+#sezione variabili globali
+numero1 = 0
+numero2 = 0
+operazione = ""
+
 #funzioni
-def operazione(testo):
-    schermo.config(text=testo)
+def operazione(testo: str):
+    if testo.isdigit():
+        #è una cifra numerica
+        vecchio_valore = valore.get() #prendo quanto scritto attualmente nella label
+        vecchio_valore += testo #aggiungo alla stringa il testo indicato
+        valore.set(vecchio_valore) #imposto la stringvar e automaticamente aggiorno la label
+    elif testo == ">":
+        vecchio_testo = valore.get()
+        vecchio_testo = vecchio_testo[:-1]
+        valore.set(vecchio_testo)
+    elif testo == ",":
+        pass
+    else:
+        #gestire l'operazione
+        global numero1
+        global operazione
+        operazione = testo
+        numero1 = valore.get()
+        valore.set("")
+
+def calcolaRisultato():
+    global numero2
+    numero2 = valore.get()
+    #operazioni
+    if operazione == "+":
+        #somma + conversione
+        n1 = float(numero1)
+        n2 = float(numero2)
+        somma = n1+n2
+        valore.set(str(somma))
+
 
 #main
 root = tk.Tk()
@@ -13,16 +47,17 @@ frm = tk.Frame(root, bg="white")
 frm.pack(fill="both", expand="true")
 
 frm_superiore = tk.Frame(frm, bg="red")
-frm_superiore.pack()
+frm_superiore.pack(fill="x")
 
 frm_inferiore = tk.Frame(frm, bg="green")
 frm_inferiore.pack()
 
 #gestione frame superiore
-uguale = tk.Button(frm_superiore, text="=")
+uguale = tk.Button(frm_superiore, text="=", command=calcolaRisultato)
 uguale.grid(column=0, row=0, sticky="W")
 
-schermo = tk.Label(frm_superiore, text="")
+valore = tk.StringVar() #variabile globale
+schermo = tk.Label(frm_superiore, textvariable=valore)
 schermo.grid(column=1, row=0, sticky="W")
 
 frm_superiore.grid_rowconfigure(0, weight=1)
